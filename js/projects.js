f775d65b4d8eebc772df9616271d9d7dac061c24
HarobMikonam <mattfaghiri@gmail.com>
2024-10-03 14:09:44 +0300

    projects page javascript

--- js/projects.js
+++ js/projects.js
@@ -2,11 +2,9 @@ class ProjectSection extends HTMLElement {
     connectedCallback() {
         const maincontainer = document.createElement('div');
         maincontainer.className = 'main-container';
-        const max_projects = 5
+        const max_projects = 8
 
-        
-
-        for (let i = 1; i <= max_projects; i++) {
+        for (let i = 0; i <= max_projects; i++) {
             const project = document.createElement('div');
             project.className = "container";
 
@@ -19,19 +17,40 @@ class ProjectSection extends HTMLElement {
             project.appendChild(box);
 
             maincontainer.appendChild(project);
-        }
+            }
         this.appendChild(maincontainer);
 
-        const boxes = document.querySelectorAll('.container'); // selecting all boxes in the container
+        // CSS BOX-BEHIND MANIPULATION
+        const container = document.querySelectorAll('.container');
+        const centerIndex = 4
 
-        const centerIndex = 4 // defining the center of index as 4, so we skip over it since it doesnt have a box-behind
+        container.forEach((box, index) => {
+            const boxBehind = box.querySelector('.box-behind');
 
-        boxes.forEach((box, index) => {
-            console.log(box.className)
+            //if its 4 skip over the middle box and remove the box behind it.
+            if (index === centerIndex) {
+                if (boxBehind) {
+                    boxBehind.remove(); 
+                }
+                return;
+            }
+            // get row and column position
+            const row = Math.floor(index / 3);
+            const column = index % 3;
+
+            // calculating how far away (rows) the box is from the center
+            const rowDistance = row - 1;
+            const columnDistance = column - 1;
+
+            //adjusting position
+            if (boxBehind){
+                boxBehind.style.top = `${-7 * rowDistance}px`;
+                boxBehind.style.left = `${-7 * columnDistance}px`;
+            }
+
         });
 
-            }
-        
+        }
     }
 
 customElements.define('project-setion', ProjectSection);
